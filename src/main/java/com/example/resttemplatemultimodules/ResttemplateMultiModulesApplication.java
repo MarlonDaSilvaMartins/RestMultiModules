a512df8db995eb449ee5375f172979c94edc732e
Marlon Martins <marlon673@hotmail.com>
2022-05-16 10:53:11 -0300

    first commit

---
+++ src/main/java/com/example/resttemplatemultimodules/ResttemplateMultiModulesApplication.java
@@ -0,0 +1,13 @@
+package com.example.resttemplatemultimodules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttemplateMultiModulesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResttemplateMultiModulesApplication.class, args);
+    }
+
+}
